d0004e2b425bd138d74486fb90ac12f65834f28a
athospugliesedev <athospugliesedev@gmail.com>
2023-02-19 18:17:17 -0300

    finish api

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreateMovieRentController } from "../modules/movies/createMovieRent/CreateMovieRentController";
 import { CreateMovieController } from "../modules/movies/useCases/createMovie/CreateMovieController";
+import { GetMoviesByReleaseDateUseController } from "../modules/movies/useCases/getMoviesByReleaseDate/GetMoviesByReleaseDateController";
 
 const createMovieController = new CreateMovieController();
+const getMoviesByReleaseDateUseController = new GetMoviesByReleaseDateUseController();
 const createMovieRentController = new CreateMovieRentController();
 
 const movieRoutes = Router();
 
 movieRoutes.post("/", createMovieController.handle);
+movieRoutes.get("/release", getMoviesByReleaseDateUseController.handle);
 movieRoutes.post("/rent", createMovieRentController.handle);
 
 export { movieRoutes };
